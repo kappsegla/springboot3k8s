8205e4c0faf61ccfc0af81e1183fb55363aff532
Martin Blomberg <martin.blomberg@outlook.com>
2024-04-19 18:27:20 +0200

    Initial commit

---
+++ src/main/java/org/fungover/springboot3k8s/EndpointsController.java
@@ -0,0 +1,36 @@
+package org.fungover.springboot3k8s;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.time.LocalDateTime;
+import java.time.ZonedDateTime;
+import java.util.Locale;
+import java.util.UUID;
+
+@RestController
+public class EndpointsController {
+
+    private final static UUID uuid = UUID.randomUUID();
+
+    @Autowired
+    private Environment environment;
+
+    @GetMapping("/")
+    public Information hello() throws UnknownHostException {
+        // Get IP Address
+        String ipAddress = InetAddress.getLocalHost().getHostAddress();
+
+        // Get Hostname
+        String hostname = InetAddress.getLocalHost().getHostName();
+
+        return new Information(ipAddress, hostname, ZonedDateTime.now(), Locale.getDefault(), uuid);
+    }
+
+    record Information(String ipAddress, String hostname, ZonedDateTime dateTime, Locale locale, UUID uuid) {
+    }
+}
---
+++ src/main/java/org/fungover/springboot3k8s/Springboot3k8sApplication.java
@@ -0,0 +1,13 @@
+package org.fungover.springboot3k8s;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot3k8sApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot3k8sApplication.class, args);
+    }
+
+}
